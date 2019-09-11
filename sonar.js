cc64776c6fb4dd0db3784bfe0a835aaf8cc22964
Milan Jovanovic <mjovanovic@croz.net>
2019-09-11 15:41:16 +0100

    Trying to get sonar working :S

---
+++ sonar.js
@@ -0,0 +1,13 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: process.env.SONAR_HOST,
+  token: process.env.SONAR_TOKEN,
+  options : {
+    'sonar.organization': process.env.SONAR_ORGANIZATION,
+    'sonar.projectKey': 'net.croz:qed-bank-ui',
+    'sonar.projectName': 'CROZ QED Bank - Frontend',
+    'sonar.sources': '.',
+    'sonar.inclusions' : 'src/**'
+  }
+}, () => {});
